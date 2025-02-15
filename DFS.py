a9093af5d6183dd6c1239baf541829570a388ba6
Arijeet Rege <132147878+arijeetRege@users.noreply.github.com>
2025-02-15 16:06:13 +0530

    DFS.py

--- DFS.py
+++ DFS.py
@@ -1,10 +1,10 @@
 # Arijeet Rege, Faculty of Technology and Engineering, The Maharaja Sayajirao University of Baroda
 # Q2 : Graph traversal, done by Depth First Search OR DFS
 def dftraversal(mygraph, node, visited):
-    if node not in visited:  # If the node has not been visited
-        print(node, end=" ~>> ")  # Print the node
-        visited.add(node)  # Mark it as visited
-        # Recursively visit all unvisited neighbors
+    if node not in visited:  # checking if we have visited the node
+        print(node, end=" ~>> ")  # Printing the node
+        visited.add(node)  # Marking it as visited
+        # Recursively visiting all unvisited neighbors
         for neighbor in mygraph[node]:
             dftraversal(mygraph, neighbor, visited)
 # Graph representation using adjacency list. Use of dictionary
@@ -15,7 +15,7 @@ mygraph = {
     'D': ['B'],
     'E': ['B']
 }
-# Set to track visited nodes
+# Set to track visited nodes, to avoid cycling
 visited = set()
 # Perform DFS starting from node 'A'
 print("DFS Traversal: ")
